4908260e301c5c38409d5339c4bc399c812b7f26
iqtp <ifqthenp@gmail.com>
2016-06-29 22:30:38 +0100

    add LO P1.3

---
+++ src/late-objects/01-introduction/p_01_03_product_of_ten/ProductOfTen.java
@@ -0,0 +1,14 @@
+package p_01_03_product_of_ten;
+
+/**
+ * Write a program that prints the product of the first
+ * ten positive integers 1 * 2 * 3 * ... * 10
+ */
+public class ProductOfTen
+{
+    public static void main(String[] args)
+    {
+        System.out.print("The product of the first positive integers is: ");
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+    }
+}
