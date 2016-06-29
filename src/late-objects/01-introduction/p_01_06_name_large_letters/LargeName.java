9971d09650bdb432263d62829f8c40635ed2b0cf
iqtp <ifqthenp@gmail.com>
2016-06-29 22:35:45 +0100

    add P1.6

---
+++ src/late-objects/01-introduction/p_01_06_name_large_letters/LargeName.java
@@ -0,0 +1,37 @@
+package p_01_06_name_large_letters;
+
+/**
+ * Write a program that prints your name in large letters.
+ */
+public class LargeName
+{
+    public static void main(String[] args)
+    {
+        System.out.println(
+                "********         *****        **************** ***************         ");
+        System.out.println(
+                "********      ***********     **************** *******************     ");
+        System.out.println(
+                "  ****     *****       *****       ******      *****         *******   ");
+        System.out.println(
+                "  ****    ****           ****      ******      *****          *******  ");
+        System.out.println(
+                "  ****    ****           ****      ******      *****          *******  ");
+        System.out.println(
+                "  ****    ****           ****      ******      *****         *******   ");
+        System.out.println(
+                "  ****    ****           ****      ******      *******************     ");
+        System.out.println(
+                "  ****    ****           ****      ******      ****************        ");
+        System.out.println(
+                "  ****     *****       *****       ******      *****                   ");
+        System.out.println(
+                "********      *************        ******      *****                   ");
+        System.out.println(
+                "********         *****   ***       ******      *****                   ");
+        System.out.println(
+                "                           **                                          ");
+        System.out.println(
+                "                             *                                         ");
+    }
+}
