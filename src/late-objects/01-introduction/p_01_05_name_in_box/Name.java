4a6e23dc8affa3075764ad5bce713bcfdaaa4831
iqtp <ifqthenp@gmail.com>
2016-06-29 22:34:42 +0100

    add LO P1.5

---
+++ src/late-objects/01-introduction/p_01_05_name_in_box/Name.java
@@ -0,0 +1,14 @@
+package p_01_05_name_in_box;
+
+/**
+ * Write a program that prints your name inside a box on the screen.
+ */
+public class Name
+{
+    public static void main(String[] args)
+    {
+        System.out.println("+------+");
+        System.out.println("| NAME |");
+        System.out.println("+------+");
+    }
+}
