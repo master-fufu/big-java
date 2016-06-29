497a8f2f237c62dd5e3fb04b783c037b59c181d2
iqtp <ifqthenp@gmail.com>
2016-06-29 22:36:00 +0100

    add LO P1.7

---
+++ src/late-objects/01-introduction/p_01_07_face/Face.java
@@ -0,0 +1,18 @@
+package p_01_07_face;
+
+/**
+ * Write a program that prints a face.
+ */
+public class Face
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  /////  ");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+
+    }
+}
