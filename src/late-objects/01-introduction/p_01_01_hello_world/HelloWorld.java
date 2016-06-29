aeb531bbb583543303423dae3a5f7f95e86b67e7
iqtp <ifqthenp@gmail.com>
2016-06-29 22:26:29 +0100

    add LO P1.1

---
+++ src/late-objects/01-introduction/p_01_01_hello_world/HelloWorld.java
@@ -0,0 +1,12 @@
+package p_01_01_hello_world;
+
+/**
+ * Write a program that prints a greeting of your choice.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
