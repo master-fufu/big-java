bbcccf4df76e565a3a899506da89547bfddd9bbb
iqtp <ifqthenp@gmail.com>
2016-06-29 22:36:20 +0100

    add LO P1.8

---
+++ src/late-objects/01-introduction/p_01_08_modrian_painting/MondrianPainting.java
@@ -0,0 +1,18 @@
+package p_01_08_modrian_painting;
+
+/**
+ * Write a program that prints an imitation of Piet Mondrian painting.
+ */
+public class MondrianPainting
+{
+    public static void main(String[] args)
+    {
+        System.out.println("_____|____|______|________|____");
+        System.out.println("     |$$$$|      |@@@@@@@@|    ");
+        System.out.println("_____|$$$$|______|@@@@@@@@|____");
+        System.out.println("     |    |      |        |    ");
+        System.out.println("     |____|      |        |    ");
+        System.out.println("_____|::::|______|________|____");
+        System.out.println("     |    |      |        |    ");
+    }
+}
