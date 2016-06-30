7aced10503630d0be550fae6996f4c6ece8f5f48
iqtp <ifqthenp@gmail.com>
2016-06-30 14:16:07 +0100

    add LO P1.10

---
+++ src/late-objects/01-introduction/p_01_10_animal/Animal.java
@@ -0,0 +1,16 @@
+package p_01_10_animal;
+
+/**
+ * Write a program that prints an animal speaking a greeting.
+ */
+public class Animal
+{
+    public static void main(String[] args)
+    {
+        System.out.println("              ------      ");
+        System.out.println(" /\\___/\\    / Hello  \\ ");
+        System.out.println("(==^.^==) <   Junior  |   ");
+        System.out.println(" (\")(\")_/   \\ Coder! / ");
+        System.out.println("              ------      ");
+    }
+}
