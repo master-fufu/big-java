1df541b0411f9e54a0baf1cce94efb42c2973399
iqtp <ifqthenp@gmail.com>
2016-06-30 14:15:51 +0100

    add LO P1.9

---
+++ src/late-objects/01-introduction/p_01_09_house/House.java
@@ -0,0 +1,18 @@
+package p_01_09_house;
+
+/**
+ * Write a program that prints a house.
+ */
+public class House
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   +   ");
+        System.out.println("  + +  ");
+        System.out.println(" +   + ");
+        System.out.println("+-----+");
+        System.out.println("| .-. |");
+        System.out.println("| | | |");
+        System.out.println("+-+-+-+");
+    }
+}
