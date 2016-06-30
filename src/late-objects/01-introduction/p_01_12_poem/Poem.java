18a8a7c4a54e7da19113a6b839b721c3d3d0ae00
iqtp <ifqthenp@gmail.com>
2016-07-01 00:05:32 +0100

    add LO P1.12

---
+++ src/late-objects/01-introduction/p_01_12_poem/Poem.java
@@ -0,0 +1,27 @@
+package p_01_12_poem;
+
+/**
+ * Write a program that prints a poem of your choice. If you don’t have a
+ * favorite poem, search the Internet for "Emily Dickinson" or "e e cummings".
+ */
+public class Poem
+{
+    public static void main(String[] args)
+    {
+        System.out.print("\"My uncle, a most worthy gentleman,\n"
+                                 + "When he fell seriously ill,\n"
+                                 + "By snuffing it made us all respect him,\n"
+                                 + "Couldn't have done better if he tried.\n"
+                                 + "His behaviour was a lesson to us all.\n"
+                                 + "But, God above, what crushing boredom\n"
+                                 + "To sit with the malingerer night and day\n"
+                                 + "Not moving even one footstep away.\n"
+                                 + "What demeaning hypocrisy\n"
+                                 + "To amuse the half-dead codger,\n"
+                                 + "To fluff up his pillows, and then,\n"
+                                 + "Mournfully to bring him his medicine;\n"
+                                 + "To think to oneself, and to sigh:\n"
+                                 + "When the devil will the old rascal die?\"\n"
+                                 + "                       Alexander Pushkin\n");
+    }
+}
