5d09fa29d274e2bd15e0a6ea732ecddf4132f7ab
iqtp <ifqthenp@gmail.com>
2016-07-01 00:06:06 +0100

    add LO P1.13

---
+++ src/late-objects/01-introduction/p_01_13_american_flag/AmericanFlag.java
@@ -0,0 +1,26 @@
+package p_01_13_american_flag;
+
+/**
+ * Write a program that prints the United States flag, using * and = characters.
+ */
+public class AmericanFlag
+{
+    public static void main(String[] args)
+    {
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * * ===================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * * ===================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * * ===================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * * ===================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
