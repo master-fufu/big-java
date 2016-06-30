f340384e2cbbe0150209816be08687abdf0b0514
iqtp <ifqthenp@gmail.com>
2016-07-01 00:04:39 +0100

    add LO P1.11

---
+++ src/late-objects/01-introduction/p_01_11_print_three_items/ThreeNames.java
@@ -0,0 +1,15 @@
+package p_01_11_print_three_items;
+
+/**
+ * Write a program that prints three items, such as the file of your three
+ * best friends or favorite movies, on three separate lines.
+ */
+public class ThreeNames
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Joseph Stalin");
+        System.out.println("Winston Churchill");
+        System.out.println("Franklin D. Roosevelt");
+    }
+}
