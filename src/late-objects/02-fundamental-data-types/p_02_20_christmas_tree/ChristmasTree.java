daccaab5e163f82ff36139d25c8edc7023d9e88a
iqtp <ifqthenp@gmail.com>
2016-07-10 19:51:41 +0100

    add LO P2.20

---
+++ src/late-objects/02-fundamental-data-types/p_02_20_christmas_tree/ChristmasTree.java
@@ -0,0 +1,19 @@
+package p_02_20_christmas_tree;
+
+/**
+ * Write a program that prints a Christmas tree. Remember to use escape sequence.
+ */
+public class ChristmasTree
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   /\\'   ");
+        System.out.println("  /  \\'  ");
+        System.out.println(" /    \\' ");
+        System.out.println("/      \\'");
+        System.out.println("--------  ");
+        System.out.println("  \"  \"  ");
+        System.out.println("  \"  \"  ");
+        System.out.println("  \"  \"  ");
+    }
+}
